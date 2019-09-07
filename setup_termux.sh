ea21131277157138926f411fd1efcdaa56b29a86
chiplukes <34627158+chiplukes@users.noreply.github.com>
2019-09-07 13:26:22 -0600

    Update setup_termux.sh

--- setup_termux.sh
+++ setup_termux.sh
@@ -3,15 +3,15 @@
 # https://www.reddit.com/r/tasker/comments/7bdtzu/running_a_python_script/
 
 apt update
-apt install git termux-tools
-apt install coreutils
-apt install python
-apt install neovim
-apt install curl
+apt install -y git termux-tools
+apt install -y coreutils
+apt install -y python
+apt install -y neovim
+apt install -y curl
 
 # neovim setup
-apt install clang
-apt install python-dev
+apt install -y clang
+apt install -y python-dev
 mkdir -p ~/.config/nvim
 mkdir -p ~/.local/share/nvim/site/autoload
 git clone https://github.com/chiplukes/init.vim.git ~/.config/nvim/cfg
@@ -23,7 +23,7 @@ mkdir -p .termux/tasker
 # start of python file needs:  #!/data/data/com.termux/files/usr/bin/env python
 # also make sure python file is executable: chmod +x ./hello.py
 
-pkg install termux-exec
+pkg install -y termux-exec
 
 # failed attempt at getting pyenv set up :(
 # curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
